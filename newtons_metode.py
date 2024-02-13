07ff32029fcaaf0f2c1c65ddd83f30b026294a6b
EStorvik <erlend.storvik@hvl.no>
2024-02-13 13:46:06 +0100

    La inn mer innfyllingsruter

--- newtons_metode.py
+++ newtons_metode.py
@@ -10,6 +10,17 @@ def dfdx(x):
 
 # ______________________
 def newtons_metode(x, f, dfdx, tol=1e-10, max_iter=100):
+    """
+    Newtons metode for å finne nullpunkt av f(x)
+
+    argumenter:
+        x: ________
+        f: ________
+        dfdx: ________
+        tol: ________
+        max_iter: ________
+    
+    Returnerer: ____, ____"""
     for i in range(max_iter):
         x = x - f(x)/dfdx(x)
         if abs(f(x)) < tol:
